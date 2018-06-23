3fb9f280828b470a1be85f9f937ed6f4409f3859
AdamMorag <moragadam@gmail.com>
2018-06-23 21:52:20 +0300

    Fixed previous check in

---
+++ premier-league-genetic-algorithm/Data/PlayerDataBase.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using premier_league_genetic_algorithm.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace premier_league_genetic_algorithm.Data
+{
+    public static class PlayerDataBase
+    {
+        public static Player[] Players;
+
+        static PlayerDataBase()
+        {
+            using (StreamReader reader = new StreamReader("./Data/DataBase.json"))
+            {
+                var dbString = reader.ReadToEnd();
+
+                Players = JsonConvert.DeserializeObject<Player[]>(dbString);                
+            }
+        }
+    }
+}
