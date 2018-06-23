ae1bbaa462e9b5577220ab3ad7b058b6fdbdf67b
AdamMorag <moragadam@gmail.com>
2018-06-23 21:28:32 +0300

    Added model and data

--- premier-league-genetic-algorithm/Controllers/ExampleController.cs
+++ premier-league-genetic-algorithm/Controllers/ExampleController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using premier_league_genetic_algorithm.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -10,9 +14,16 @@ namespace premier_league_genetic_algorithm.Controllers
     public class ExampleController : ApiController
     {
         [Route("HelloWorld")]
-        public string GetExample()
+        public async Task<int> GetExample()
         {
-            return "hello world";
+            using (StreamReader reader = new StreamReader("./Data/DataBase.json"))
+            {
+                var dbString = await reader.ReadToEndAsync();
+
+                var players = JsonConvert.DeserializeObject<GenericPlayer[]>(dbString);
+
+                return players.Count();
+            }
         }
     }
 }
